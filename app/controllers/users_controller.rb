70cc0f09da9c03cdae3bc9679d2afdc1b308883b
Takuya <dyhpaax@gmail.com>
2019-03-30 13:20:20 +0900

    Finish ch08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
   end
 
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
       # GET "/users/#{@user.id}" => show
